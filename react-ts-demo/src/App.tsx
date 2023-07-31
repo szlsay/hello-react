775351003838c5a28582248453c7b5dad30a7fe0
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-31 16:47:43 +0800

    list1

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,39 +1,14 @@
 import React from "react";
 
 import "./App.css";
-import "./List1.css";
-function App() {
-  // 问卷列表数据
-  const questionList = [
-    { id: "q1", title: "问卷1", isPublished: false },
-    { id: "q2", title: "问卷2", isPublished: true },
-    { id: "q3", title: "问卷3", isPublished: false },
-    { id: "q4", title: "问卷4", isPublished: true },
-  ];
+import List1 from "./List1";
 
-  function edit(value: string) {
-    console.log(value);
-  }
+function App() {
+
 
   return (
     <div className="App">
-      <div>
-        {questionList.map((item) => {
-          return (
-            <div key={item.id} className="list-item">
-              <strong>{item.title}</strong>
-              {item.isPublished ? <span>已发布</span> : <span>未发布</span>}
-              <button
-                onClick={() => {
-                  edit(item.id);
-                }}
-              >
-                编辑{" "}
-              </button>
-            </div>
-          );
-        })}
-      </div>
+      <List1></List1>
     </div>
   );
 }
---
+++ react-ts-demo/src/List1.tsx
@@ -0,0 +1,37 @@
+import React, {FC} from "react";
+// import type { FC } from 'react'
+import "./List1.css";
+const List1: FC = () => {
+    // 问卷列表数据
+    const questionList = [
+      { id: "q1", title: "问卷1", isPublished: false },
+      { id: "q2", title: "问卷2", isPublished: true },
+      { id: "q3", title: "问卷3", isPublished: false },
+      { id: "q4", title: "问卷4", isPublished: true },
+    ];
+  
+    function edit(value: string) {
+      console.log(value);
+    }
+  return (
+    <div>
+    {questionList.map((item) => {
+      return (
+        <div key={item.id} className="list-item">
+          <strong>{item.title}</strong>
+          {item.isPublished ? <span>已发布</span> : <span>未发布</span>}
+          <button
+            onClick={() => {
+              edit(item.id);
+            }}
+          >
+            编辑{" "}
+          </button>
+        </div>
+      );
+    })}
+  </div>
+  )
+}
+
+export default List1

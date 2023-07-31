e8041c8d461c653b75b776de5634636cfd04d1fa
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-07-31 15:28:09 +0800

    jsx

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # hello-react
 前端框架的核心价值: 组件化 和 数据驱动视图
 
+## 2、JSX语法
+标签
+首字母小写 - HTML tag, 首字母大写为自定义组件 如 <input/> 和 <Input/> 就不一样
+JSX 里的标签必须是闭合的，<input> <br> 这样写在 JSX 会报错（在 HTML 中不会报错），必须闭合 <input/>
+每一段 JSX 只能有一个根节点，或者使用 <></> （ Fragment ）
+
+属性
+和 HTML 属性基本一样
+class 要改为 className
+style 要写成 JS 对象（不能是 string），key 采用驼峰写法
+for 要改为 htmlFor
+
 ## vite安装
 https://cn.vitejs.dev/
 
--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const list = (
+  <div></div>
+)
+const temp = (
+  <></>
+)
 
 function App() {
+  const a = 0;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <span>{a}</span>
         <p>
           Edit <code>src/App.tsx</code> and save to reload stapp.
         </p>
+        <div>
+          <label htmlFor="name">姓名</label>
+          <input type="text" id='name' />
+        </div>
+        <div className='box'>
+        </div>
+        <div style={{width: '10px', height: "10px", backgroundColor: "green"}}></div>
         <a
           className="App-link"
           href="https://reactjs.org"

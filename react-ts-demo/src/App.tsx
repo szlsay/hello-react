21215883df7326c81b29741f63f2c03626085e6d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-01 14:06:30 +0800

    ahook

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 
 import "./App.css";
-import List1 from "./List1";
-import List2 from "./List2";
-import Immer1 from "./Immer1";
-import UseRefDemo from "./UseRefDemo";
-import UseMemoDemo from "./UseMemoDemo";
-import UseCallbackDemo from "./UseCallbackDemo";
-import useTitle from "./hooks/useTitle";
-import useMouse from "./hooks/useMouse";
-import useGetInfo from "./hooks/useGetInfo";
+// import List1 from "./List1";
+// import List2 from "./List2";
+// import Immer1 from "./Immer1";
+// import UseRefDemo from "./UseRefDemo";
+// import UseMemoDemo from "./UseMemoDemo";
+// import UseCallbackDemo from "./UseCallbackDemo";
+// import useTitle from "./hooks/useTitle";
+// import useMouse from "./hooks/useMouse";
+// import useGetInfo from "./hooks/useGetInfo";
+
+import { useTitle } from "ahooks";
 function App() {
   // useTitle("App page")
   // const {x, y} = useMouse()
   // const {loading, info} = useGetInfo();
+  useTitle("测试");
   return (
     <div className="App">
       {/* <List1></List1> */}

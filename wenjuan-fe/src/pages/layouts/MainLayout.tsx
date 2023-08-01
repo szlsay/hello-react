3d0bd05ae22898586bc22d009255872be50d5c82
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-01 17:46:53 +0800

    fe

---
+++ wenjuan-fe/src/pages/layouts/MainLayout.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const MainLayout: FC = () => {
+  return <div></div>;
+};
+
+export default MainLayout;
---
+++ wenjuan-fe/src/pages/layouts/ManageLayout.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const ManageLayout: FC = () => {
+  return (
+    <></>
+  )
+}
+
+export default ManageLayout

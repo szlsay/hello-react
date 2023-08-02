a007f017d074f676e5618f51911abd99b84b4935
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-02 09:44:29 +0800

    antd

--- wenjuan-fe/src/pages/Home.tsx
+++ wenjuan-fe/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { Button } from 'antd';
 const Home: FC = () => {
   const nav = useNavigate();
   const goLogin = () => {
@@ -19,6 +20,7 @@ const Home: FC = () => {
       <button onClick={goLogin}>goLogin</button>
       <button onClick={goLogin1}>goLogin</button>
       <Link to='/register'>register</Link>
+      <Button type="primary">Button</Button>
     </>
   );
 };

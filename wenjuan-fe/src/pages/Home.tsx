b655cd253298b7830667b59e5181b4ce8c322ffe
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-02 16:42:41 +0800

    craco

--- wenjuan-fe/src/pages/Home.tsx
+++ wenjuan-fe/src/pages/Home.tsx
@@ -1,16 +1,23 @@
-import React, { FC, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Button, Typography } from 'antd'
-import { MANAGE_INDEX_PATHNAME } from '../router'
-import styles from './Home.module.scss'
+import React, { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Typography } from "antd";
+import { MANAGE_INDEX_PATHNAME } from "../router";
+import styles from "./Home.module.scss";
 
-// import axios from 'axios'
+import axios from "axios";
 // import '../_mock/index.ts'
 
-const { Title, Paragraph } = Typography
+const { Title, Paragraph } = Typography;
 
 const Home: FC = () => {
-  const nav = useNavigate()
+  const nav = useNavigate();
+
+  useEffect(() => {
+    // axios 内部使用 XMLHttpRequest API ，没用 fetch
+    axios
+      .get("http://localhost:3001/api/question")
+      .then((res) => console.log("axios data", res.data));
+  }, []);
 
   // useEffect(() => {
   //   // fetch('/api/test')
@@ -21,7 +28,6 @@ const Home: FC = () => {
   //   // axios 内部使用 XMLHttpRequest API ，没用 fetch
   //   // axios.get('/api/test').then(res => console.log('axios data', res.data))
   // }, [])
-
 
   // function clickHandler() {
   //   // nav('/login')
@@ -35,7 +41,9 @@ const Home: FC = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <Title>问卷调查 | 在线投票</Title>
-        <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
+        <Paragraph>
+          已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份
+        </Paragraph>
         <div>
           <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
             开始使用
@@ -43,7 +51,7 @@ const Home: FC = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

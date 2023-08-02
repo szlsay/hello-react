3bd85d51efc854ff3c9b04392010a86291660a59
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-02 09:28:14 +0800

    ahooks

--- wenjuan-fe/src/pages/question/Edit/index.tsx
+++ wenjuan-fe/src/pages/question/Edit/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
-
+import { useTitle } from 'ahooks'
 const Edit: FC = () => {
   const { id } = useParams();
+  useTitle(`问卷编辑 - `)
   return <>Edit {id}</>;
 };
 

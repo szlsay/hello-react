b72f287a95ec9a4721d100e9bbf40742cffcf0ab
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-02 17:18:02 +0800

    mock

--- wenjuan-fe/src/pages/question/Edit/index.tsx
+++ wenjuan-fe/src/pages/question/Edit/index.tsx
@@ -1,9 +1,18 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useTitle } from 'ahooks'
+import { useTitle } from "ahooks";
+import { getQuestionService } from "../../../services/question";
 const Edit: FC = () => {
-  const { id } = useParams();
-  useTitle(`问卷编辑 - `)
+  const { id = "" } = useParams();
+  useTitle(`问卷编辑 - `);
+
+  useEffect(() => {
+    async function fn() {
+      const data = await getQuestionService(id);
+      console.log(data);
+    }
+    fn();
+  }, []);
   return <>Edit {id}</>;
 };
 

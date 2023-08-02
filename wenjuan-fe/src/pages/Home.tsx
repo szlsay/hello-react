1b3618c95b425da94e5df47a0f94f7a765d96be6
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-02 10:35:28 +0800

    Update Home.tsx

--- wenjuan-fe/src/pages/Home.tsx
+++ wenjuan-fe/src/pages/Home.tsx
@@ -1,28 +1,55 @@
-import { FC } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { Button } from 'antd';
+import React, { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import styles from './Home.module.scss'
+
+// import axios from 'axios'
+// import '../_mock/index.ts'
+
+const { Title, Paragraph } = Typography
+
 const Home: FC = () => {
-  const nav = useNavigate();
-  const goLogin = () => {
-    const pathname = "/login";
-    nav({
-      pathname,
-      search: 'id=1'
-    });
-  };
+  const nav = useNavigate()
 
-  const goLogin1 = () => {
-    nav('login?id=1')
-  };
+  // useEffect(() => {
+  //   // fetch('/api/test')
+  //   //   .then(res => res.json())
+  //   //   .then(data => console.log('fetch data', data))
+  //   // mock.js 只能劫持 XMLHttpRequest ，不能劫持 fetch
+
+  //   // axios 内部使用 XMLHttpRequest API ，没用 fetch
+  //   axios.get('/api/test').then(res => console.log('axios data', res.data))
+  // }, [])
+
+  // useEffect(() => {
+  //   // fetch('/api/test')
+  //   //   .then(res => res.json())
+  //   //   .then(data => console.log('fetch data', data))
+  //   // axios.get('/api/test').then(res => console.log('axios data', res.data))
+  // })
+
+  // function clickHandler() {
+  //   // nav('/login')
+  //   nav({
+  //     pathname: '/login',
+  //     search: 'b=21',
+  //   })
+  // }
+
   return (
-    <>
-      home
-      <button onClick={goLogin}>goLogin</button>
-      <button onClick={goLogin1}>goLogin</button>
-      <Link to='/register'>register</Link>
-      <Button type="primary">Button</Button>
-    </>
-  );
-};
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+            开始使用
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Home;
+export default Home

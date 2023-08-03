09ce0ded0e02fc6590d091e82eb5b87f29e3b663
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-03 16:15:53 +0800

    mobx

--- mobx-demo/src/App.tsx
+++ mobx-demo/src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import BasicDemo from './pages/BasicDemo'
+import TodoList from './pages/TodoList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>MobX demo</h1>
+      {/* <BasicDemo/> */}
+      <TodoList/>
+    </>
   );
 }
 

5adc6878c6a6d60c437bce0725e6cce0f58a9591
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-03 14:28:44 +0800

    redux

---
+++ redux-demo/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Count from './pages/Count'
+import TodoList from './pages/TodoList';
+
+function App() {
+  return (
+    <>
+      <h1>Redux demo</h1>
+      <Count/>
+      <hr/>
+      <TodoList/>
+    </>
+  );
+}
+
+export default App;
+

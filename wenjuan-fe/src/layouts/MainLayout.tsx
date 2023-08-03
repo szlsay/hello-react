393790919c06accd6d0e3c7797a45f04a6c9b4c3
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-03 17:22:26 +0800

    Update MainLayout.tsx

--- wenjuan-fe/src/layouts/MainLayout.tsx
+++ wenjuan-fe/src/layouts/MainLayout.tsx
@@ -34,7 +34,7 @@ const MainLayout: FC = () => {
           )}
         </Content>
       </Layout>
-      <Footer className={styles.footer}>小慕问卷 &copy;2023 - present. Created by 双越老师</Footer>
+      <Footer className={styles.footer}>问卷 &copy;2023 - present. Created by ST</Footer>
     </Layout>
   )
 }

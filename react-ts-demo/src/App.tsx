15c8e72d203852a355fadac9400cfbcc599c32f5
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-03 12:56:37 +0800

    useReducer

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,10 +1,12 @@
-import ContextDemo from "./ContextDemo"
-import CountReducer from "./CountReducer";
+// import ContextDemo from "./ContextDemo"
+// import CountReducer from "./CountReducer";
+import TodoReducer from "./TodoReducer";
 export default function App() {
   return (
     <div>
       {/* <ContextDemo></ContextDemo> */}
-      <CountReducer></CountReducer>
+      {/* <CountReducer></CountReducer> */}
+      <TodoReducer></TodoReducer>
     </div>
   );
 }

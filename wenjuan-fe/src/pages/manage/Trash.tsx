5e131bab2ca4262d9cd22f510de63907c4a8f553
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-08-03 09:43:04 +0800

    Update Trash.tsx

--- wenjuan-fe/src/pages/manage/Trash.tsx
+++ wenjuan-fe/src/pages/manage/Trash.tsx
@@ -1,124 +1,118 @@
-import React, { FC, useState } from "react";
-import { useTitle } from "ahooks";
-import {
-  Typography,
-  Empty,
-  Table,
-  Tag,
-  Button,
-  Space,
-  Modal,
-  Spin,
-  message,
-} from "antd";
-import ListSearch from "../../components/ListSearch";
-import styles from "./common.module.scss";
+import React, { FC, useState } from 'react'
+import { useTitle } from 'ahooks'
+import { Typography, Empty, Table, Tag, Button, Space, Modal, Spin, message } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+import { useRequest } from 'ahooks'
+import ListSearch from '../../components/ListSearch'
+// import ListPage from '../../components/ListPage'
+import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
+import { updateQuestionService, deleteQuestionsService } from '../../services/question'
+import styles from './common.module.scss'
 
-const { Title } = Typography;
-const { confirm } = Modal;
-const rawList = [
-  {
-    _id: "q1",
-    title: "问卷1",
-    isPublished: false,
-    isStar: false,
-    answerCount: 5,
-    createdAt: "2023年8月1日16:40:19",
-  },
-  {
-    _id: "q2",
-    title: "问卷2",
-    isPublished: true,
-    isStar: false,
-    answerCount: 5,
-    createdAt: "2023年8月1日16:40:19",
-  },
-  {
-    _id: "q3",
-    title: "问卷3",
-    isPublished: true,
-    isStar: false,
-    answerCount: 5,
-    createdAt: "2023年8月1日16:40:19",
-  },
-  {
-    _id: "q4",
-    title: "问卷4",
-    isPublished: true,
-    isStar: true,
-    answerCount: 50,
-    createdAt: "2013年8月1日16:40:19",
-  },
-];
+const { Title } = Typography
+const { confirm } = Modal
+
 const Trash: FC = () => {
-  useTitle("回收站");
-  const [list, setList] = useState(rawList);
+  useTitle('小慕问卷 - 回收站')
+
+  const { data = {}, loading, refresh } = useLoadQuestionListData({ isDeleted: true })
+  const { list = [], total = 0 } = data
+
   // 记录选中的 id
-  const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [selectedIds, setSelectedIds] = useState<string[]>([])
+
+  // 恢复
+  const { run: recover } = useRequest(
+    async () => {
+      for await (const id of selectedIds) {
+        await updateQuestionService(id, { isDeleted: false })
+      }
+    },
+    {
+      manual: true,
+      debounceWait: 500, // 防抖
+      onSuccess() {
+        message.success('恢复成功')
+        refresh() // 手动刷新列表
+        setSelectedIds([])
+      },
+    }
+  )
+
+  // 删除
+  const { run: deleteQuestion } = useRequest(
+    async () => await deleteQuestionsService(selectedIds),
+    {
+      manual: true,
+      onSuccess() {
+        message.success('删除成功')
+        refresh()
+        setSelectedIds([])
+      },
+    }
+  )
+
+  function del() {
+    confirm({
+      title: '确认彻底删除该问卷？',
+      icon: <ExclamationCircleOutlined />,
+      content: '删除以后不可以找回',
+      onOk: deleteQuestion,
+    })
+  }
+
   const tableColumns = [
     {
-      title: "标题",
-      dataIndex: "title",
+      title: '标题',
+      dataIndex: 'title',
       // key: 'title', // 循环列的 key ，它会默认取 dataIndex 的值
     },
     {
-      title: "是否发布",
-      dataIndex: "isPublished",
+      title: '是否发布',
+      dataIndex: 'isPublished',
       render: (isPublished: boolean) => {
-        return isPublished ? (
-          <Tag color="processing">已发布</Tag>
-        ) : (
-          <Tag>未发布</Tag>
-        );
+        return isPublished ? <Tag color="processing">已发布</Tag> : <Tag>未发布</Tag>
       },
     },
     {
-      title: "答卷",
-      dataIndex: "answerCount",
+      title: '答卷',
+      dataIndex: 'answerCount',
     },
     {
-      title: "创建时间",
-      dataIndex: "createdAt",
+      title: '创建时间',
+      dataIndex: 'createdAt',
     },
-  ];
+  ]
 
   // 可以把 JSX 片段定义为一个变量
   const TableElem = (
     <>
-      <div style={{ marginBottom: "16px" }}>
+      <div style={{ marginBottom: '16px' }}>
         <Space>
-          <Button
-            type="primary"
-            disabled={selectedIds.length === 0}
-            // onClick={recover}
-          >
+          <Button type="primary" disabled={selectedIds.length === 0} onClick={recover}>
             恢复
           </Button>
-          <Button
-            danger
-            disabled={selectedIds.length === 0}
-            // onClick={del}
-          >
+          <Button danger disabled={selectedIds.length === 0} onClick={del}>
             彻底删除
           </Button>
         </Space>
       </div>
-      <div style={{ border: "1px solid #e8e8e8" }}>
+      <div style={{ border: '1px solid #e8e8e8' }}>
         <Table
           dataSource={list}
           columns={tableColumns}
           pagination={false}
-          rowKey={(q) => q._id}
+          rowKey={q => q._id}
           rowSelection={{
-            type: "checkbox",
-            onChange: (selectedRowKeys) => {
-              setSelectedIds(selectedRowKeys as string[]);
+            type: 'checkbox',
+            onChange: selectedRowKeys => {
+              setSelectedIds(selectedRowKeys as string[])
             },
           }}
         />
       </div>
     </>
-  );
+  )
 
   return (
     <>
@@ -131,12 +125,19 @@ const Trash: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
-        {list.length === 0 && <Empty description="暂无数据" />}
+        {loading && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
+        {!loading && list.length === 0 && <Empty description="暂无数据" />}
         {list.length > 0 && TableElem}
       </div>
-      <div className={styles.footer}></div>
+      <div className={styles.footer}>
+        {/* <ListPage total={total} /> */}
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default Trash;
+export default Trash
